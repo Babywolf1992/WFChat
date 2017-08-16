d37ee7b51e22b4dd7dcbc875ceedc7581f00678d
babywolf <591138406@qq.com>
2017-08-16 17:00:08 +0800

    Create project and import xmppframework

---
+++ WFChat/WFChat/Common.h
@@ -0,0 +1,16 @@
+//
+//  Common.h
+//  WFChat
+//
+//  Created by babywolf on 17/8/16.
+//  Copyright © 2017年 babywolf. All rights reserved.
+//
+
+#ifndef Common_h
+#define Common_h
+
+#define kUsername   @"username"
+#define kPassword   @"password"
+#define kHost   @"host"
+
+#endif /* Common_h */
